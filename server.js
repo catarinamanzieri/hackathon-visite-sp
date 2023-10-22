23acac3189f2e346d625c71bf8515aeb62672019
catarinamanzieri <catarina.manzieri@gmail.com>
2023-10-22 04:44:29 -0300

    enviar mensagens e identificar usuario

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile); // engine ejs serve para que o nodejs crie páginas dinâmicas além das limitações do html
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.use('/', (req, res) => {
     res.render('index.html');
+});
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`)
+
+    socket.on('enviarMensagem', data => {
+        console.log(data);
+    });
 });
 
 server.listen(3000);
